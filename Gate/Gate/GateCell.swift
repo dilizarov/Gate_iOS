0c0a9730e01edab41114fc7d77061f2c70e5c517
David Ilizarov <dilizarov@gmail.com>
2015-03-14 02:16:58 -0500

    Fixed dynamic label height

--- Gate/Gate/GateCell.swift
+++ Gate/Gate/GateCell.swift
@@ -36,12 +36,12 @@ class GateCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        self.layoutIfNeeded()
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
 
